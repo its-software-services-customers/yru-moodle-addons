0d51653075a9e888e2bcca463d28da821c7b862d
pjames <pjames@gmail.com>
2023-08-17 11:28:53 +0700

    Fixed script

--- 01-init-cluster.bash
+++ 01-init-cluster.bash
@@ -5,4 +5,5 @@ export $(xargs <.env)
 cd "01-init"
 
 helm dependency update
-helm install . -f values-${ENV}.yaml -n argocd --generate-name --create-namespace
+#helm install . -f values-${ENV}.yaml -n argocd --generate-name --create-namespace
+helm upgrade -i r1 . -f values-${ENV}.yaml -n argocd
