31f89cf85bf8043fc249bcfc73d2d562ad905ffa
pjames <pjames@gmail.com>
2023-08-17 14:36:04 +0700

    Refactored

--- 04-cluster-monitoring.bash
+++ 04-cluster-monitoring.bash
@@ -5,6 +5,6 @@ export $(xargs <.env)
 cd "04-monitoring"
 
 helm dependency update
-helm upgrade -i monitoring . -f values-${ENV}.yaml -n monitoring
+helm upgrade -i monitoring . -f values-${ENV}.yaml -n monitoring --create-namespace
 
 kubectl apply -f extra/alm-config.yaml -n monitoring
