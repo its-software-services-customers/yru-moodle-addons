c8e74075bda412916c3604ad2a62c2b9e82a0ece
pjames <pjames@gmail.com>
2023-08-17 11:07:31 +0700

    Use --create-namespace

--- 01-init-cluster.bash
+++ 01-init-cluster.bash
@@ -5,4 +5,4 @@ export $(xargs <.env)
 cd "01-init"
 
 helm dependency update
-helm install . -f values-${ENV}.yaml -n argocd --generate-name
+helm install . -f values-${ENV}.yaml -n argocd --generate-name --create-namespace
